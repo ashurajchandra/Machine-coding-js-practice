2b608fcd899d535157abac0d5e924ddf9c08aaea
ashurajchandra <mukutrai12@gmail.com>
2023-11-10 23:34:28 +0530

    rating

---
+++ star-rating/index.js
@@ -0,0 +1,46 @@
+const stars =document.querySelector(".star-wrapper");
+const currentRating = document.querySelector(".current-rating-value");
+
+
+let filled =0;
+function handleRatings(e){
+if(filled){
+    for(let i=0; i<filled; i++){
+        stars.children[i].classList.remove("yellow")
+    }
+}
+for(let i =0 ; i<e.target.id; i++){
+    const star = stars.children[i]
+    star.classList.add("yellow")
+}
+filled = e.target.id
+currentRating.textContent = e.target.id
+}
+
+function handleMouseEnter(e){
+    for(let i=0; i<filled;i++){
+        stars.children[i].classList.remove("yellow")
+    }
+    for(let i=0; i<e.target.id;i++){
+       stars.children[i].classList.add("yellow")
+       
+    }
+}
+
+function handleMouseLeave(){
+    for(let i=0; i<stars.children.length;i++){
+        stars.children[i].classList.remove("yellow")
+    }
+    for(let i=0; i<filled;i++){
+        stars.children[i].classList.add("yellow")
+    }
+}
+
+const starItems =[...stars.children]
+starItems.forEach((star)=>star.addEventListener("mouseenter",handleMouseEnter))
+starItems.forEach((star)=>star.addEventListener("mouseleave",handleMouseLeave))
+
+
+stars.addEventListener("click", handleRatings)
+// stars.addEventListener("mouseenter",handleMouseEnter)
+// stars.addEventListener("mouseleave",handleMouseLeave)
