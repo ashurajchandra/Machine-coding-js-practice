bbe73a1cf6e9070213ddd6976a4d8d272ec4fa38
ashurajchandra <mukutrai12@gmail.com>
2023-11-13 20:05:16 +0530

    added opening only current click accordion

--- accordion/index.js
+++ accordion/index.js
@@ -4,9 +4,14 @@ const accordionContent = document.querySelectorAll("[data-accordion-content]")
 
 function handleExpandAccordion(expand){
     expand.classList.toggle("transform")
-    const accordionContent= document.querySelectorAll("[data-accordion-content]")
+    let accordionContent= document.querySelectorAll("[data-accordion-content]")
+    accordionContent=[...accordionContent]
     const currentAccordionContent = accordionContent[expand.dataset.expandTarget-1]
+  const filteredAccordion=  accordionContent.filter(item=> item.dataset.accordionContent !== currentAccordionContent.dataset.accordionContent )
     currentAccordionContent.classList.toggle("active")
+    filteredAccordion.forEach((item)=>{
+        item.classList.remove("active")
+    })
 }
 
 
